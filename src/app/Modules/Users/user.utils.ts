132ed34ec8ee5117b8326aaf1df9e9075c429651
munir79 <jakirhossainmunir79@gmail.com>
2025-05-18 11:17:23 +0600

    generated Student ID

--- src/app/Modules/Users/user.utils.ts
+++ src/app/Modules/Users/user.utils.ts
@@ -1,6 +1,7 @@
 import { TAcademicSemistar } from '../AcademicSemistar/AcademicSemistar.interface';
 import { User } from './Users.model';
 
+//find last student id
 const findLastStudentId = async () => {
   const lastStudent = await User.findOne(
     {
@@ -16,12 +17,32 @@ const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generatedStudenrId = async (payLoad: TAcademicSemistar) => {
   // firstitme 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString(); // 0000  by default  [ step 1-----by default set 0]
+
+  const lastStudentId = await findLastStudentId(); // find last student id by sort
+  const LastStudentSemestarCode = lastStudentId?.substring(4, 6); //  semistar code 01 , 02
+  const lastStudentYear = lastStudentId?.substring(0, 4); // year 2030
+
+  const currentStudentSemistarCode = payLoad.code; // current semistarcode
+  const currentYear = payLoad.year.toString(); // current year
+
+  if (
+    lastStudentId &&
+    LastStudentSemestarCode === currentStudentSemistarCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
+  // if(await findLastStudentId()){
+  //  const lastStudent
+  // }
+
   let IncrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   IncrementId = `${payLoad.year} ${payLoad.code} ${IncrementId}`;
   return IncrementId;
